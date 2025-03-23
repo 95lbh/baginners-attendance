98ee9c22245fce9322180859cba82e7a0781b8a4
bhlee <zzccbbmm23@gmail.com>
2025-03-23 11:42:30 +0900

    update firebase api key

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCXV-bdoMpFHqSfgwm6NSWWOoVGjbjVGvQ",
-    authDomain: "maru-attendance.firebaseapp.com",
-    projectId: "maru-attendance",
-    storageBucket: "maru-attendance.firebasestorage.app",
-    messagingSenderId: "606697404775",
-    appId: "1:606697404775:web:63d5f871398cbf4ba617c2",
-    measurementId: "G-LW3DWKRFR5"
-  };
+  apiKey: "AIzaSyCAz7CLRCc_68A99EBF6Bp3pmBjpKX3IPo",
+  authDomain: "baginners.firebaseapp.com",
+  projectId: "baginners",
+  storageBucket: "baginners.firebasestorage.app",
+  messagingSenderId: "663458871797",
+  appId: "1:663458871797:web:9701c002503f7b4df688bc",
+  measurementId: "G-EE3VDF3KED"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
